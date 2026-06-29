a23868fe80ec2a7288214f915ae8d532b9e4766e
SamiGaming <salito1405@gmail.com>
2026-06-29 15:12:49 -0500

    v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.xd.misviejos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
